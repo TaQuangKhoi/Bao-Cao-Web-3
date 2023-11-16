064d84ad86fb8f43715b6d0241621c101f5c035b
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-16 22:25:57 +0700

    Get username and password from form.cleaned_data

--- signin/views.py
+++ signin/views.py
@@ -6,6 +6,13 @@ from signin.forms import SignInForm
 def signin(request):
     form = SignInForm()
 
+    if request.method == 'POST':
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            username = cleaned_data['username']
+            password = cleaned_data['password']
+
     return render(
         request,
         template_name='signin.jinja',
