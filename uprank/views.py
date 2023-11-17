ba50def8c497fe9a5240888e42f1c7cdb9350b7c
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-18 01:43:48 +0700

    Done select Position

--- uprank/views.py
+++ uprank/views.py
@@ -5,11 +5,13 @@ from library_db.models import Rank
 def up_rank(request):
     # get ranks with name distinct
     ranks = Rank.objects.values('name').distinct()
-    rank_position = Rank.objects.values('position').distinct()
-    print(rank_position)
+    rank_positions = Rank.objects.values('position').distinct()
 
     return render(
         request,
         template_name='up_rank.jinja',
-        context={'ranks': ranks}
+        context={
+            'ranks': ranks,
+            'rank_positions': rank_positions,
+        }
     )
