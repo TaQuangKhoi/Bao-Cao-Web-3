f4d61a482bca61ee744cf5de41e4d71c516f2030
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-17 21:06:10 +0700

    Done restore table Request

--- uprank/admin.py
+++ uprank/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import (
     Request,
 )
-# Register your models here.
+
+
 class RequestAdmin(admin.ModelAdmin):
     list_display = ['id',
                     # 'server',
--- uprank/models.py
+++ uprank/models.py
@@ -1,24 +1,39 @@
+import uuid
+from django.contrib.auth.models import User
+
 from django.db import models
 
-# Create your models here.
+
 class Request(models.Model):
-    id = models.AutoField(primary_key=True)
-    # server = models.ForeignKey('Server', on_delete=models.CASCADE)
-    # current_rank = models.ForeignKey('Rank', on_delete=models.CASCADE, related_name='current_rank', null=True)
-    # desired_rank = models.ForeignKey('Rank', on_delete=models.CASCADE, related_name='desired_rank', null=True)
-    # employee = models.ForeignKey('Employees', on_delete=models.CASCADE)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    server = models.ForeignKey('library_db.Server', on_delete=models.CASCADE, null=True)
+
+    current_rank = models.ForeignKey('library_db.Rank', on_delete=models.CASCADE, related_name='current_rank',
+                                     null=True)
+    desired_rank = models.ForeignKey('library_db.Rank', on_delete=models.CASCADE, related_name='desired_rank',
+                                     null=True)
+
     details = models.CharField(max_length=200)
     total = models.FloatField()
     status = models.CharField(max_length=200)
+
     stream_games = models.BooleanField(default=False)
     priority_order = models.BooleanField(default=False)
+
     desired_mastery = models.IntegerField()
     current_mastery = models.IntegerField()
+
     number_of_wins = models.IntegerField()
     number_of_games = models.IntegerField()
+
     ranked_genre = models.IntegerField()
     lp_Gain = models.IntegerField()
     current_lp = models.FloatField(max_length=99)
+
     request_recipient = models.DateTimeField()
     request_creation_date = models.DateTimeField()
+
     the_require_price = models.FloatField()
+
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
