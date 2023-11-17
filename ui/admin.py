3744f60d22f66dbdcfb0c7f2ae9e91c5e5a98ee3
NguyenQuangLuan606 <luannguyenquang007@gmail.com>
2023-11-17 13:43:47 +0700

    delete table request app ui

--- ui/admin.py
+++ ui/admin.py
@@ -1,26 +1,2 @@
 from django.contrib import admin
-from .models import (
-    Request,
-)
 
-
-class RequestAdmin(admin.ModelAdmin):
-    list_display = ['id',
-                    # 'server',
-                    # 'current_rank',
-                    # 'desired_rank',
-                    'details', 'total',
-                    'status', 'stream_games', 'priority_order', 'desired_mastery', 'current_mastery',
-                    'number_of_wins', 'number_of_games', 'ranked_genre', 'lp_Gain', 'current_lp',
-                    'request_recipient', 'request_creation_date', 'the_require_price']
-
-    search_fields = [
-        # 'server', 'current_rank', 'desired_rank',
-        'details', 'total',
-        'status', 'stream_games', 'priority_order', 'desired_mastery', 'current_mastery',
-        'number_of_wins', 'number_of_games', 'ranked_genre', 'lp_Gain', 'current_lp',
-        'request_recipient', 'request_creation_date', 'the_require_price'
-    ]
-
-
-admin.site.register(Request, RequestAdmin)
