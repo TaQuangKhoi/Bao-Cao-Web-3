17e009dfe363e98205767c30f87251fbe026365a
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-17 12:41:16 +0700

    Copy modal from app `ui` to app `library_db`

--- library_db/models.py
+++ library_db/models.py
@@ -1,3 +1,24 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Rank(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    position = models.CharField(max_length=50)
+
+
+class Server(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    local = models.CharField(max_length=50)
+
+
+class GameType(models.Model):
+    id = models.AutoField(primary_key=True)
+    release_date = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50)
+    publisher = models.CharField(max_length=50)
+    developer = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
