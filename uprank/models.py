2d8f60a5876e69ecf691ea0b4dbbde86d107518a
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-18 08:39:00 +0700

    Update modal allow null for all col

--- uprank/models.py
+++ uprank/models.py
@@ -9,7 +9,7 @@ class Request(models.Model):
 
     current_rank = models.ForeignKey('library_db.Rank', on_delete=models.CASCADE, related_name='current_rank',
                                      null=True)
-    current_lp = models.FloatField(max_length=99)
+    current_lp = models.FloatField(max_length=99, null=True)
     lp_Gain = models.IntegerField()
 
     desired_rank = models.ForeignKey('library_db.Rank', on_delete=models.CASCADE,
@@ -17,25 +17,25 @@ class Request(models.Model):
                                      null=True)
     server = models.ForeignKey('library_db.Server', on_delete=models.CASCADE, null=True)
 
-    details = models.CharField(max_length=200)
+    details = models.CharField(max_length=200, null=True)
     total = models.FloatField()
-    status = models.CharField(max_length=200)
+    status = models.CharField(max_length=200, null=True)
 
     priority_order = models.BooleanField(default=False)
     stream_games = models.BooleanField(default=False)
 
-    desired_mastery = models.IntegerField()
-    current_mastery = models.IntegerField()
+    desired_mastery = models.IntegerField(null=True)
+    current_mastery = models.IntegerField(null=True)
 
-    number_of_wins = models.IntegerField()
-    number_of_games = models.IntegerField()
+    number_of_wins = models.IntegerField(null=True)
+    number_of_games = models.IntegerField(null=True)
 
-    ranked_genre = models.IntegerField()
+    ranked_genre = models.IntegerField(null=True)
 
-    request_recipient = models.DateTimeField()
-    request_creation_date = models.DateTimeField()
+    request_recipient = models.DateTimeField(null=True)
+    request_creation_date = models.DateTimeField(null=True)
 
-    the_require_price = models.FloatField()
+    the_require_price = models.FloatField(null=True)
 
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
